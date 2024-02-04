155563dfcc2437da5b63f980ff9a449ddfbdc54c
Irfan Rasheed <97159197+irfanrasheedkc@users.noreply.github.com>
2024-02-04 12:17:59 +0530

    Time: 508 ms (13.87%), Space: 17.2 MB (52.55%) - LeetHub

--- 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.py
+++ 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.py
@@ -6,5 +6,5 @@ class Solution:
                 for j in range(i-k,i+k+1):
                     if j>=0 and j<len(nums):
                         if j not in map:
-                            map[j] = 0
+                            map[j] = 1
         return map
